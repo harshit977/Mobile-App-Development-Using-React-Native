00038126b4c16bec6ded0c046829389a032abb7d
Harshit Sharma <harshitsharmabtp@gmail.com>
2020-11-18 11:58:10 +0530

    activity indicator

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -3,6 +3,7 @@ import {Text, ScrollView,FlatList} from 'react-native';
 import {Card,ListItem} from 'react-native-elements';
 import {connect } from 'react-redux';
 import {baseUrl} from '../shared/baseUrl';
+import { Loading } from './LoadingComponent';
 
 const mapStateToProps = state => {
     return {
@@ -32,21 +33,51 @@ class Aboutus extends Component {
             );
         }
 
-        return(
-            <ScrollView>
-            <Card title='Our History'>
+        if(this.props.leaders.isLoading) {
+            return(
+                <ScrollView>
+                    <Card title='Our History'>
                 <Text style={{ margin: 10 }}>Started in 2010, Ristorante con Fusion quickly established itself as a culinary icon par excellence in Hong Kong. With its unique brand of world fusion cuisine that can be found nowhere else, it enjoys patronage from the A-list clientele in Hong Kong.  Featuring four of the best three-star Michelin chefs in the world, you never know what will arrive on your plate the next time you visit us.</Text>
                 <Text style={{ margin: 10 }}>The restaurant traces its humble beginnings to The Frying Pan, a successful chain started by our CEO, Mr. Peter Pan, that featured for the first time the world's best cuisines in a pan.</Text>
             </Card>
-            <Card title='Corporate Leadership'>
-            <FlatList 
-                data={this.props.leaders.leaders}
-                renderItem={Renderleader}
-                keyExtractor={item => item.id.toString()}    
-                />
-            </Card>
-            </ScrollView>
-        );
+                    <Card title='Corporate Leadership'>
+                        <Loading />
+                    </Card>
+                </ScrollView>
+            );
+        }
+        else if(this.props.leaders.errMess)
+        {
+            return(
+                <ScrollView>
+                    <Card title='Our History'>
+                <Text style={{ margin: 10 }}>Started in 2010, Ristorante con Fusion quickly established itself as a culinary icon par excellence in Hong Kong. With its unique brand of world fusion cuisine that can be found nowhere else, it enjoys patronage from the A-list clientele in Hong Kong.  Featuring four of the best three-star Michelin chefs in the world, you never know what will arrive on your plate the next time you visit us.</Text>
+                <Text style={{ margin: 10 }}>The restaurant traces its humble beginnings to The Frying Pan, a successful chain started by our CEO, Mr. Peter Pan, that featured for the first time the world's best cuisines in a pan.</Text>
+                    </Card>
+                    <Card title='Corporate Leadership'>
+                        <Text>{this.props.leaders.errMess}</Text>
+                    </Card>
+                </ScrollView>
+            );
+        }
+        else {
+            return(
+                <ScrollView>
+                <Card title='Our History'>
+                    <Text style={{ margin: 10 }}>Started in 2010, Ristorante con Fusion quickly established itself as a culinary icon par excellence in Hong Kong. With its unique brand of world fusion cuisine that can be found nowhere else, it enjoys patronage from the A-list clientele in Hong Kong.  Featuring four of the best three-star Michelin chefs in the world, you never know what will arrive on your plate the next time you visit us.</Text>
+                    <Text style={{ margin: 10 }}>The restaurant traces its humble beginnings to The Frying Pan, a successful chain started by our CEO, Mr. Peter Pan, that featured for the first time the world's best cuisines in a pan.</Text>
+                </Card>
+                <Card title='Corporate Leadership'>
+                <FlatList 
+                    data={this.props.leaders.leaders}
+                    renderItem={Renderleader}
+                    keyExtractor={item => item.id.toString()}    
+                    />
+                </Card>
+                </ScrollView>
+            );
+        }
+        
     }
 }
 
--- components/MenuComponent.js
+++ components/MenuComponent.js
@@ -4,6 +4,7 @@ import { Tile } from 'react-native-elements';
 import { DISHES } from '../shared/dishes';
 import {connect } from 'react-redux';
 import {baseUrl} from '../shared/baseUrl';
+import { Loading } from './LoadingComponent';
 
 const mapStateToProps = state => {
     return {
@@ -36,13 +37,28 @@ class Menu extends Component {
 
         const { navigate } = this.props.navigation;
 
-        return (
-            <FlatList 
-                data={this.props.dishes.dishes}
-                renderItem={renderMenuItem}
-                keyExtractor={item => item.id.toString()}    //this is supplied as index to the rendermenuitem
-                />
-              );
+        if(this.props.dishes.isLoading) {
+            return(
+                <Loading />
+            );
+        }
+        else if(this.props.dishes.errMess) {
+            return(
+                <View>
+                   <Text>{this.props.dishes.errMess}</Text> 
+                </View>
+            );
+        }
+        else {
+            return (
+                <FlatList 
+                    data={this.props.dishes.dishes}
+                    renderItem={renderMenuItem}
+                    keyExtractor={item => item.id.toString()}    //this is supplied as index to the rendermenuitem
+                    />
+                  );
+        }
+        
     }
   
 }
